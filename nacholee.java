459b19d6b8b8a519707ef8824e7a11b4bc09a533
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-02-09 09:15:47 -0500

    holiss

---
+++ nacholee.java
@@ -0,0 +1,16 @@
+package Estructuras21;
+
+import java.util.Scanner;
+
+public class nacholee {
+    public static void main(String[] args){
+        System.out.println("Hola Mundo");
+        int n1=0, n2 =0;
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Ingrese el numero 1");
+        n1= sc.nextInt();
+        System.out.print("Ingrese el numero 2");
+        n2= sc.nextInt();
+        System.out.println("La suma de los numeros es");
+    }
+}
